6f528ee7989a01845b8e5af8c02f1503ae2dbd33
Benjamin Lang <>
2025-12-28 17:58:35 +0100

    i managed to build a jar!!!

--- src/test/java/com/benny/icalculation/application/ConfigTest.java
+++ src/test/java/com/benny/icalculation/application/ConfigTest.java
@@ -1,13 +1,15 @@
 package com.benny.icalculation.application;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConfigTest {
 
     @Test
     void getInvalidateCacheAfterSeconds() {
-        assertEquals(Config.getInvalidateCacheAfterSeconds(), Config.INVALIDATE_CACHE_AFTER_SECONDS);
+        Assertions.assertEquals(Config.getInvalidateCacheAfterSeconds(), Config.INVALIDATE_CACHE_AFTER_SECONDS);
     }
 }
